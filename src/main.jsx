e23a0bc98e6d747de759013226ec5f447471e249
MartinB1202 <18258610@uagro.mx>
2025-10-30 17:01:50 -0600

    jajajaj

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './App.css'
-import LoginForm from './assets/components/Login.jsx'
+import LoginForm from './components/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
